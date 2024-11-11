5ad2b7dc12bba09153e5b94f0aede1dc9686ca85
Ozmacun <149619575+Ozmacun@users.noreply.github.com>
2024-11-11 23:56:52 +0300

    Codes written and tested for all possibilities + challenges

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lamp lamp = new Lamp(LampType.LAVA,true,8);
+        Bed bed = new Bed("King-Size",2, 2,2,3);
+        Ceiling ceiling = new Ceiling(5,PaintColor.WHITE);
+        Wardrobe wardrobe = new Wardrobe(3,4,150);
+        Wall wall1 = new Wall("South");
+        Wall wall2 = new Wall("West");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("North");
+        Wall[] walls = new Wall[]{wall1, wall2, wall3, wall4};
+        Room room1 = new Room(wall1, wall2, wall3, wall4, ceiling, lamp, new Carpet(3, 2,PaintColor.RED));
+        Room room2 = new Room(wall1, wall2, wall3, wall4, ceiling, lamp);
+        Bedroom bedroom = new Bedroom(wall1, wall2, wall3, wall4, ceiling,lamp,new Carpet(4, 6 ,PaintColor.WHITE),"Parent",bed,wardrobe);
+
+        bedroom.createBedroom();
     }
 }
